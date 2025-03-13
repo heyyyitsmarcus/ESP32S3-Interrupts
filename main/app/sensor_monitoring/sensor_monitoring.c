6b37d6a9382d6363efd658915c94b333636155ac
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-03-12 21:33:18 -0700

    Adjusted sensor_monitoring to work with new bme280 driver. Adjusted functions to not rely on global variables, rather the device instances can be passed in.

--- main/app/sensor_monitoring/sensor_monitoring.c
+++ main/app/sensor_monitoring/sensor_monitoring.c
@@ -1,26 +1,16 @@
 #include "sensor_monitoring.h"
 
-void collect_sensor_data(bme_measurement_t* bme_data, pms5003_measurement_t* pms_data, environmental_reading_t* sensor_readings)
+#include "driver/pms5003/pms5003.h"
+#include "driver/bme280/bme280.h"
+#include "driver/bme280/bme280_defs.h"
+
+#include "node_config.h"
+
+
+void collect_sensor_data( sensors_handles_t* sensors, environmental_reading_t* reading )
 {
-    pms5003_make_measurement(&pms0, pms_data);
-    bme_make_measurement(bme_data);
-    initReading(sensor_readings, bme_data, pms_data);
-}
-
-
-void initReading(environmental_reading_t* sensorReadings, const bme_measurement_t* bmeReading,
-                 const pms5003_measurement_t* pmsReading) 
-{
-    sensorReadings->temperature = bmeReading->temperature;
-    sensorReadings->pressure = bmeReading->pressure;
-    sensorReadings->humidity = bmeReading->humidity;
-
-    sensorReadings->pm1_0_std = pmsReading->pm1_0_std;
-    sensorReadings->pm2_5_std = pmsReading->pm2_5_std;
-    sensorReadings->pm10_std = pmsReading->pm10_std;
-    sensorReadings->pm1_0_atm = pmsReading->pm1_0_atm;
-    sensorReadings->pm2_5_atm = pmsReading->pm2_5_atm;
-    sensorReadings->pm10_atm = pmsReading->pm10_atm;
+    pms5003_make_measurement( sensors->pms_handle, &reading->pms );
+    bme280_get_sensor_data( BME280_ALL, &reading->bme, sensors->bme_handle );
 }
 
 
--- main/app/sensor_monitoring/sensor_monitoring.h
+++ main/app/sensor_monitoring/sensor_monitoring.h
@@ -1,53 +1,43 @@
 #ifndef SENSOR_MONITORING_H
 #define SENSOR_MONITORING_H
 
-#include "esp_log.h"
-
-#include "driver/bme280/bme_task.h"
 #include "driver/pms5003/pms5003.h"
-#include "driver/lora/lora.h"
-
-#include "app/packet_assembly/packet_assembly.h"
-#include "globals/globals.h"
+#include "driver/bme280/bme280_defs.h"
 
 
-// Collect sensor data from queues and initialize the environmental_reading_t struct.
-//
-// @param bme_data : struct to hold the bme data coming in from the queue.
-//
-// @param pms_data : struct to hold the pms data coming in from the queue.
-//
-// @param mq7_data : struct to hold the mq7 data coming in from the queue.
-//
-// @param sensor_readings : struct to consolidate each of the sensor datas
-// received from the queue.
-void collect_sensor_data(bme_measurement_t* bme_data,
-                         pms5003_measurement_t* pms_data,
-                         environmental_reading_t* sensor_readings);
+// Environmental readings from each sensor
+typedef struct
+{
+    struct bme280_data      bme;
+    pms5003_measurement_t   pms;
+} environmental_reading_t;
 
 
-// Initialize a environmental_reading_t struct from sensor data
+// Handles to set of sensor devices
+typedef struct
+{
+    struct bme280_dev*  bme_handle;
+    pms5003_config_t*   pms_handle;
+} sensors_handles_t;
+
+
+// Collect sensor data and initialize the environmental_reading_t struct.
 //
-// @param sensorReadings : empty environmental_reading_t struct to be initalized.
+// sensors : struct containing pointers to device instances for use in measurement functions
 //
-// @param bmeReading : bme sensor data.
-//
-// @param pmsReading : pms sensor data.
-//
-// @param mq7Reading : mq7 sensor data.
-void initReading(environmental_reading_t* sensorReadings, const bme_measurement_t* bmeReading,
-                 const pms5003_measurement_t* pmsReading);
+// reading : struct to hold data from each sensor
+void collect_sensor_data( sensors_handles_t* sensors, environmental_reading_t* reading );
 
 
 // Validate that the mac address in the received packet matches the sensor node.
 //
-// @param mac : mac address of sensor node.
+// mac                      : mac address of sensor node.
 //
-// @param received_packet : packet recieved through lora.
+// received_packet          : packet recieved through lora.
 //
-// @param received_packet_length : lenght of packet.
+// received_packet_length   : lenght of packet.
 //
-// return : 1 for true, 0 for false.
+// return                   : 1 for true, 0 for false.
 //
 int validate_mac(uint8_t* mac, uint8_t* received_packet, int received_packet_length);
 
